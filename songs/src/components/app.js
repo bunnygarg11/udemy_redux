4c4e5530e92daf89494f069788f5922a373257d5
bunnygarg11 <mohitgarg083@gmail.com>
2019-11-11 00:19:16 +0530

    lec 150

--- songs/src/components/app.js
+++ songs/src/components/app.js
@@ -1,16 +1,20 @@
 import React from "react"
 import SongList from "./SongList"
+import SongDetail from './SongDetail';
 
-// console.log(SelectSong)
-const App=()=>{
-    return (<div className="ui container grid">
-        <div className="ui row">
-            <div className="column eight wide">
-                <SongList/>
-            </div>
+const App = () => {
+  return (
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
         </div>
-    </div>)
+        <div className="column eight wide">
+          <SongDetail />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default App
+export default App;
